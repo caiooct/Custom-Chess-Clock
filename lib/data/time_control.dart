9bea34333fc18eb11f6e4c74d2a1e17c2237e613
caiooctavio <pkaiow@gmail.com>
2023-02-05 16:45:08 -0400

    Add model class: TimeControl

---
+++ lib/data/time_control.dart
@@ -0,0 +1,13 @@
+import 'timing_methods_enum.dart';
+
+class TimeControl {
+  final int timeInSeconds;
+  final TimingMethodEnum timingMethod;
+  final int incrementInSeconds;
+
+  const TimeControl({
+    required this.timeInSeconds,
+    required this.timingMethod,
+    required this.incrementInSeconds,
+  });
+}
