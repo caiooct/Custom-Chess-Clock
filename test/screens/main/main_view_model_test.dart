dd35f6b6306c2d2bfab3c6102f64ff7995b95640
caiooctavio <pkaiow@gmail.com>
2023-02-22 18:48:17 -0400

    Refactor unit tests that use Future.delayed to use FakeAsync

--- test/screens/main/main_view_model_test.dart
+++ test/screens/main/main_view_model_test.dart
@@ -65,25 +65,30 @@ void main() {
         });
 
         test("should pause", () async {
-          var whiteTimeBeforePause = viewModel.whiteTimer.inSeconds;
-          var blackTimeBeforePause = viewModel.blackTimer.inSeconds;
-          viewModel.pauseGame();
-          expect(viewModel.timer.isActive, isFalse);
-          expect(viewModel.gameState, GameState.paused);
-          expect(viewModel.turn.isWhite, isTrue);
-          expect(viewModel.turn.isBlack, isFalse);
-          await Future.delayed(const Duration(seconds: 2));
-          expect(whiteTimeBeforePause, equals(viewModel.whiteTimer.inSeconds));
-          expect(blackTimeBeforePause, equals(viewModel.blackTimer.inSeconds));
+          fakeAsync((async) {
+            var whiteTimeBeforePause = viewModel.whiteTimer.inSeconds;
+            var blackTimeBeforePause = viewModel.blackTimer.inSeconds;
+            viewModel.pauseGame();
+            expect(viewModel.timer.isActive, isFalse);
+            expect(viewModel.gameState, GameState.paused);
+            expect(viewModel.turn.isWhite, isTrue);
+            expect(viewModel.turn.isBlack, isFalse);
+            async.elapse(const Duration(seconds: 2));
+            expect(whiteTimeBeforePause, equals(viewModel.whiteTimer.inSeconds));
+            expect(blackTimeBeforePause, equals(viewModel.blackTimer.inSeconds));
+          });
         });
 
         test("should end the game", () async {
-          await Future.delayed(const Duration(seconds: blackTimeInSeconds + 1));
-          expect(viewModel.timer.isActive, isFalse);
-          expect(viewModel.gameState, GameState.ended);
-          expect(viewModel.whiteTimer, lessThanOrEqualTo(Duration.zero));
-          expect(viewModel.turn.isBlack, isFalse);
-          expect(viewModel.turn.isWhite, isFalse);
+          fakeAsync((async) {
+            viewModel.startGame();
+            async.elapse(const Duration(seconds: whiteTimeInSeconds + 1));
+            expect(viewModel.timer.isActive, isFalse);
+            expect(viewModel.gameState, GameState.ended);
+            expect(viewModel.whiteTimer, lessThanOrEqualTo(Duration.zero));
+            expect(viewModel.turn.isBlack, isFalse);
+            expect(viewModel.turn.isWhite, isFalse);
+          });
         });
 
         test("should increment in fischer timing method", () {
@@ -106,14 +111,16 @@ void main() {
         });
 
         test("should resume the game", () async {
-          viewModel.resumeGame();
-          expect(viewModel.timer.isActive, isTrue);
-          expect(viewModel.gameState, GameState.running);
-          expect(viewModel.turn.isWhite, isTrue);
-          expect(viewModel.turn.isBlack, isFalse);
-          await Future.delayed(const Duration(seconds: 2));
-          expect(whiteTimeAfterPause, isNot(viewModel.whiteTimer.inSeconds));
-          expect(blackTimeAfterPause, equals(viewModel.blackTimer.inSeconds));
+          fakeAsync((async) {
+            viewModel.resumeGame();
+            expect(viewModel.timer.isActive, isTrue);
+            expect(viewModel.gameState, GameState.running);
+            expect(viewModel.turn.isWhite, isTrue);
+            expect(viewModel.turn.isBlack, isFalse);
+            async.elapse(const Duration(seconds: 2));
+            expect(whiteTimeAfterPause, isNot(viewModel.whiteTimer.inSeconds));
+            expect(blackTimeAfterPause, equals(viewModel.blackTimer.inSeconds));
+          });
         });
       });
     });
@@ -134,25 +141,31 @@ void main() {
         });
 
         test("should pause", () async {
-          var whiteTimeBeforePause = viewModel.whiteTimer.inSeconds;
-          var blackTimeBeforePause = viewModel.blackTimer.inSeconds;
-          viewModel.pauseGame();
-          expect(viewModel.timer.isActive, isFalse);
-          expect(viewModel.gameState, GameState.paused);
-          expect(viewModel.turn.isWhite, isFalse);
-          expect(viewModel.turn.isBlack, isTrue);
-          await Future.delayed(const Duration(seconds: 2));
-          expect(whiteTimeBeforePause, equals(viewModel.whiteTimer.inSeconds));
-          expect(blackTimeBeforePause, equals(viewModel.blackTimer.inSeconds));
+          fakeAsync((async) {
+            var whiteTimeBeforePause = viewModel.whiteTimer.inSeconds;
+            var blackTimeBeforePause = viewModel.blackTimer.inSeconds;
+            viewModel.pauseGame();
+            expect(viewModel.timer.isActive, isFalse);
+            expect(viewModel.gameState, GameState.paused);
+            expect(viewModel.turn.isWhite, isFalse);
+            expect(viewModel.turn.isBlack, isTrue);
+            async.elapse(const Duration(seconds: 2));
+            expect(whiteTimeBeforePause, equals(viewModel.whiteTimer.inSeconds));
+            expect(blackTimeBeforePause, equals(viewModel.blackTimer.inSeconds));
+          });
         });
 
         test("should end the game", () async {
-          await Future.delayed(const Duration(seconds: blackTimeInSeconds + 1));
-          expect(viewModel.timer.isActive, isFalse);
-          expect(viewModel.gameState, GameState.ended);
-          expect(viewModel.blackTimer, lessThanOrEqualTo(Duration.zero));
-          expect(viewModel.turn.isBlack, isFalse);
-          expect(viewModel.turn.isWhite, isFalse);
+          fakeAsync((async) {
+            viewModel.startGame();
+            viewModel.onPressedTimerButton(true);
+            async.elapse(const Duration(seconds: blackTimeInSeconds + 1));
+            expect(viewModel.timer.isActive, isFalse);
+            expect(viewModel.gameState, GameState.ended);
+            expect(viewModel.blackTimer, lessThanOrEqualTo(Duration.zero));
+            expect(viewModel.turn.isBlack, isFalse);
+            expect(viewModel.turn.isWhite, isFalse);
+          });
         });
 
         test("should increment in fischer timing method", () {
@@ -176,14 +189,16 @@ void main() {
         });
 
         test("should resume the game", () async {
-          viewModel.resumeGame();
-          expect(viewModel.timer.isActive, isTrue);
-          expect(viewModel.gameState, GameState.running);
-          expect(viewModel.turn.isWhite, isFalse);
-          expect(viewModel.turn.isBlack, isTrue);
-          await Future.delayed(const Duration(seconds: 2));
-          expect(whiteTimeAfterPause, equals(viewModel.whiteTimer.inSeconds));
-          expect(blackTimeAfterPause, isNot(viewModel.blackTimer.inSeconds));
+          fakeAsync((async) {
+            viewModel.resumeGame();
+            expect(viewModel.timer.isActive, isTrue);
+            expect(viewModel.gameState, GameState.running);
+            expect(viewModel.turn.isWhite, isFalse);
+            expect(viewModel.turn.isBlack, isTrue);
+            async.elapse(const Duration(seconds: 2));
+            expect(whiteTimeAfterPause, equals(viewModel.whiteTimer.inSeconds));
+            expect(blackTimeAfterPause, isNot(viewModel.blackTimer.inSeconds));
+          });
         });
       });
     });
