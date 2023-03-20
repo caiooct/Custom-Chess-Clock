c74350b31825eaa88bdeba4aaab5aec75655c942
caiooctavio <pkaiow@gmail.com>
2023-03-19 21:42:20 -0400

    Add initial reactivity to NewClockScreen
    - add controllers to simple clock textfields
    - move _PlayerConfigurationWidget to another file
    - add a validation to time textfields not be > 59
    - set and retrieve values for both player of clock

--- lib/ui/new_clock/new_clock_screen.dart
+++ lib/ui/new_clock/new_clock_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../../../common/extensions/on_duration.dart';
 
+import '../../common/extensions/on_int.dart';
+import '../../common/services/isar_service.dart';
+import '../../data/data_sources/clock_local_data_source.dart';
 import '../../data/enums/timing_methods_enum.dart';
+import '../../data/models/clock.dart';
+import '../../data/repositories/clock_repository.dart';
+import 'new_clock_view_model.dart';
 
 part './widgets/_timer_input_widget.dart';
+
+part './widgets/_player_configuration_widget.dart';
 
 class NewClockScreen extends StatefulWidget {
   const NewClockScreen({Key? key}) : super(key: key);
@@ -15,12 +24,20 @@ class NewClockScreen extends StatefulWidget {
 class _NewClockScreenState extends State<NewClockScreen>
     with TickerProviderStateMixin {
   late TabController tabController;
+  final TextEditingController _nameTextController = TextEditingController();
+  final NewClockViewModel _viewModel = NewClockViewModel(
+    ClockRepository(ClockLocalDataSource(IsarService.instance)),
+  );
+
   ClockTypeEnum clockType = ClockTypeEnum.simple;
 
   @override
   void initState() {
     super.initState();
     tabController = TabController(length: 2, vsync: this);
+    _nameTextController.addListener(() {
+      _viewModel.clock.name = _nameTextController.text;
+    });
   }
 
   @override
@@ -40,10 +57,11 @@ class _NewClockScreenState extends State<NewClockScreen>
       ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
             child: TextField(
-              decoration: InputDecoration(
+              controller: _nameTextController,
+              decoration: const InputDecoration(
                 label: Text("Name"),
                 border: OutlineInputBorder(),
               ),
@@ -95,239 +113,21 @@ class _NewClockScreenState extends State<NewClockScreen>
               child: TabBarView(
                 controller: tabController,
                 children: [
-                  _PlayerConfigurationView(type: clockType),
-                  _PlayerConfigurationView(type: clockType),
+                  _PlayerConfigurationWidget(
+                    type: clockType,
+                    viewModel: _viewModel,
+                    isPlayerOne: true,
+                  ),
+                  _PlayerConfigurationWidget(
+                    type: clockType,
+                    viewModel: _viewModel,
+                    isPlayerOne: false,
+                  ),
                 ],
               ),
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class _PlayerConfigurationView extends StatefulWidget {
-  const _PlayerConfigurationView({Key? key, required this.type})
-      : super(key: key);
-
-  final ClockTypeEnum type;
-
-  @override
-  State<_PlayerConfigurationView> createState() =>
-      _PlayerConfigurationViewState();
-}
-
-class _PlayerConfigurationViewState extends State<_PlayerConfigurationView> {
-  TimingMethodEnum timingMethod = TimingMethodEnum.delay;
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final textTheme = Theme.of(context).textTheme;
-    return SingleChildScrollView(
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (widget.type == ClockTypeEnum.simple) ...[
-              Row(
-                children: [
-                  Text("Time", style: textTheme.titleMedium),
-                  const Spacer(),
-                  Row(
-                    children: const [
-                      _TimerInputWidget(),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 2.0),
-                        child: Text(":"),
-                      ),
-                      _TimerInputWidget(),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 2.0),
-                        child: Text(":"),
-                      ),
-                      _TimerInputWidget(),
-                    ],
-                  ),
-                ],
-              ),
-            ] else ...[
-              Text("Stages", style: textTheme.titleMedium),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.remove, color: colorScheme.error),
-                    onPressed: () {},
-                  ),
-                  Text("1", style: textTheme.bodyMedium),
-                  const SizedBox(width: 8.0),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const _TimerInputWidget(),
-                        Text("moves in", style: textTheme.bodyMedium),
-                        Row(
-                          children: const [
-                            _TimerInputWidget(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 2.0),
-                              child: Text(":"),
-                            ),
-                            _TimerInputWidget(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 2.0),
-                              child: Text(":"),
-                            ),
-                            _TimerInputWidget(),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.remove, color: colorScheme.error),
-                    onPressed: () {},
-                  ),
-                  Text("2", style: textTheme.bodyMedium),
-                  const SizedBox(width: 8.0),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const SizedBox(),
-                        Text("game in", style: textTheme.bodyMedium),
-                        Row(
-                          children: const [
-                            _TimerInputWidget(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 2.0),
-                              child: Text(":"),
-                            ),
-                            _TimerInputWidget(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 2.0),
-                              child: Text(":"),
-                            ),
-                            _TimerInputWidget(),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.add, color: Colors.green),
-                    onPressed: () {},
-                  ),
-                  Text("add stage", style: textTheme.bodyMedium),
-                ],
-              ),
-            ],
-            const SizedBox(height: 24),
-            Text("Timing method", style: textTheme.titleMedium),
-            const SizedBox(height: 8),
-            Center(
-              child: SegmentedButton<TimingMethodEnum>(
-                segments: const [
-                  ButtonSegment(
-                    value: TimingMethodEnum.delay,
-                    label: Text("Delay"),
-                  ),
-                  ButtonSegment(
-                    value: TimingMethodEnum.bronstein,
-                    label: Text("Bronstein"),
-                  ),
-                  ButtonSegment(
-                    value: TimingMethodEnum.fischer,
-                    label: Text("Fischer"),
-                  ),
-                ],
-                selected: {timingMethod},
-                onSelectionChanged: (newSelection) {
-                  setState(() {
-                    timingMethod = newSelection.first;
-                  });
-                },
-                showSelectedIcon: false,
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    const EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 16,
-                    ),
-                  ),
-                  backgroundColor: MaterialStateColor.resolveWith(
-                    (states) => states.contains(MaterialState.selected)
-                        ? colorScheme.primary
-                        : colorScheme.background,
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child:
-                  Text(timingMethod.description, style: textTheme.labelSmall),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Text("Increment", style: textTheme.titleMedium),
-                const Spacer(),
-                Row(
-                  children: const [
-                    _TimerInputWidget(),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 2.0),
-                      child: Text(":"),
-                    ),
-                    _TimerInputWidget(),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Text(
-                  "Same configuration for both players",
-                  style: textTheme.bodySmall,
-                ),
-                const Spacer(),
-                Switch(value: true, onChanged: (_) {}),
-              ],
-            ),
-            const SizedBox(height: 32),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: colorScheme.primary,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Save",
-                    style: textTheme.labelLarge?.copyWith(
-                      color: colorScheme.onPrimary,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
--- lib/ui/new_clock/widgets/_timer_input_widget.dart
+++ lib/ui/new_clock/widgets/_timer_input_widget.dart
@@ -1,7 +1,9 @@
 part of '../new_clock_screen.dart';
 
 class _TimerInputWidget extends StatelessWidget {
-  const _TimerInputWidget({Key? key}) : super(key: key);
+  final TextEditingController controller;
+
+  const _TimerInputWidget(this.controller, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +11,15 @@ class _TimerInputWidget extends StatelessWidget {
     return SizedBox(
       width: (2 * (textTheme?.fontSize ?? 16)),
       child: TextField(
+        controller: controller,
         maxLength: 2,
         keyboardType: TextInputType.number,
         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        onChanged: (value) {
+          if ((int.tryParse(value) ?? 0) > 60) {
+            controller.text = "59";
+          }
+        },
         textAlign: TextAlign.center,
         style: textTheme,
         decoration: const InputDecoration(
